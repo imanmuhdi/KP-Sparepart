29653d710ac1a9ad46ff707b1aa336e68dd1dee7
Alexander Tommy Kurniawan <ktommy475@gmail.com>
2021-08-12 14:39:46 +0700

    Update Perbaikan Mesin3

--- application/controllers/Perbaikan.php
+++ application/controllers/Perbaikan.php
@@ -32,7 +32,6 @@ class Perbaikan extends CI_Controller {
             redirect("Home");
         }
         $this->load->model("PerbaikanModel","",TRUE);
-
         $data['tb_sparepart'] = $this->PerbaikanModel->getPerbaikan1();
         $data['tb_mesin'] = $this->PerbaikanModel->getPerbaikan2();
         $this->load->view("user/perbaikanMesin2",$data);
@@ -40,9 +39,25 @@ class Perbaikan extends CI_Controller {
 
     public function prosesTambah() {
         $this->load->model("PerbaikanModel","",TRUE);
+        $loop = false;
+        $loop2 = false;
+        $row1 = 0;
+        $row2 = 0;
+        while($loop != TRUE && $loop2 != TRUE){
+            if($this->PerbaikanModel->getPerbaikan2("id_mesin")->row($row1)->id_mesin == $this->input->post("id")){
+                    $loop = TRUE;
+                }else{
+                    $row1 = $row1 + 1;
+                }
+             if($this->PerbaikanModel->getPerbaikan1("kd_part")->row($row2)->kd_part == $this->input->post("kd")){
+                    $loop2 = TRUE;
+                }else{
+                    $row2 = $row2 + 1;
+                }
+        }
         $perbaikan = array(
-            "id_mesin" => $this->PerbaikanModel->getPerbaikan2("id_mesin")->row()->id_mesin,
-            "kd_part" => $this->PerbaikanModel->getPerbaikan1("kd_part")->row()->kd_part,
+            "id_mesin" => $this->PerbaikanModel->getPerbaikan2("id_mesin")->row($row1)->id_mesin,
+            "kd_part" => $this->PerbaikanModel->getPerbaikan1("kd_part")->row($row2)->kd_part,
             "deskripsi" => $this->input->post("deskripsi")
         );
         if($this->PerbaikanModel->insertPerbaikan($perbaikan)) {
