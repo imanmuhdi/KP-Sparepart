13086c87d2e723e3fe4c6008d62f8c2e0fdf3971
Alexander Tommy Kurniawan <ktommy475@gmail.com>
2021-11-07 11:41:49 +0700

    penambahan data eksport pdf tabel perbaikan dengan sparepart

--- application/views/laporan_pdf.php
+++ application/views/laporan_pdf.php
@@ -6,9 +6,9 @@
 				'table_open' => '<table id="myTable" border=1 width="100%">'
 				);
 				$this->table->set_template($template); 
-				$this->table->set_heading("No","Id Mesin","Kode Part","Jumlah Part", "Deskripsi");
+				$this->table->set_heading("No","Id Mesin","Merk Mesin","Kode part","Type part","Jumlah Part", "Masalah","Penyebab","Hasil Perbaikan","Lokasi","Diajukan","Tanggal","Mulai","Selesai","Down Time","Disetujui","Dilaksakan");
 				foreach($tb_perbaikan1 as $r){
-				$this->table->add_row($r->no,$r->id_mesin,$r->kd_part,$r->jml_part,$r->deskripsi);
+				$this->table->add_row($r->no,$r->id_mesin,$r->merk_m,$r->kd_part,$r->type,$r->jml_part,$r->deskripsi,$r->deskripsi2,$r->hasil,$r->lokasi,$r->oleh,$r->tgl,$r->mulai,$r->selesai,$r->d_time,$r->penyetuju,$r->pelaksana);
 				}
 				echo $this->table->generate();		
 			?>
